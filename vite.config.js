157137b9fd767b07828d1b5b0de14d83988ed98a
artiomleo <artiomleo@gmail.com>
2024-01-11 12:06:37 +0200

    chore: replace rollup with vite

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue2';
+
+const isProd = process.env.BUILD === 'production'
+console.log('isProd!!!!!!!!!', isProd)
+const target = process.env.BUILD_TARGET;
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  build: {
+    outDir: `static/js/${target}`,
+    emptyOutDir: false,
+    rollupOptions: {
+      external: ['vue', 'Highcharts', 'wretch'],
+      output: {
+        globals: {
+          wretch: 'wretch',
+          vue: 'Vue',
+          highcharts: 'Highcharts'
+        },
+        entryFileNames: `index.js`,
+        format: 'iife',
+        sourcemap: !isProd
+      },
+      input: `static/js/${target}/index.vue`,
+    }
+  }
+});
